2a3fd458ba6c3310b1996d8d651ce3d95ace37be
lampthithawat <d.thithawat@gmail.com>
2024-08-06 14:04:31 +0700

    import

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mx-6 my-10">
+    <v-row class="mb-4">
+      <v-col>
+        <v-btn class="mr-2" color="blue">
+          เพิ่ม Part
+        </v-btn>
+        <v-btn class="mr-2" color="green">
+          Import
+        </v-btn>
+        <v-btn class="mr-2" color="orange">
+          Export
+        </v-btn>
+      </v-col>
+    </v-row>
+    
+    <v-data-table
+      :headers="headers"
+      class="elevation-1"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComponent',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    headers: [
+      {
+        text: "Part No",
+        value: "Part",
+      }
+    ]
+  })
+}
+</script>
