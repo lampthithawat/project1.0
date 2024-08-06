af1a419133c80c47def1fcaf3e2cfef9dc6d3b2a
lampthithawat <d.thithawat@gmail.com>
2024-08-06 14:10:21 +0700

    database

---
+++ databasepj1/databasepj1.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 06, 2024 at 08:48 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `databasepj1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `part`
+--
+
+CREATE TABLE `part` (
+  `part_id` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `partminute`
+--
+
+CREATE TABLE `partminute` (
+  `part_1` varchar(11) NOT NULL,
+  `part_2` varchar(11) NOT NULL,
+  `minute` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `part`
+--
+ALTER TABLE `part`
+  ADD PRIMARY KEY (`part_id`);
+
+--
+-- Indexes for table `partminute`
+--
+ALTER TABLE `partminute`
+  ADD PRIMARY KEY (`part_1`,`part_2`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
